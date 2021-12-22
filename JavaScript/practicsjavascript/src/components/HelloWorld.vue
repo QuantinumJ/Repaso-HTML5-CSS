fca1e4f7bcbdbd241182421fa60b229666966270
QuantinumJ <eduardvarnavskiy@yahoo.es>
2021-12-22 15:03:45 +0100

    practicamos

---
+++ JavaScript/practicsjavascript/src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+// Manejop basico de Strings
+let s ="saludo, Eduard";
+// sustrae posiciones de 1 a 4
+console.log(s.substring(1,4));
+console.log(s.slice(1,4)); //Lo mismo 
+console.log(s.slice(-3)) // Por la derecha las 3 ultimas posiciones
+console.log(s.split(", ")) // rompe el array por el separrador
+
+// Buscador en la fila
+console.log(s.indexOf("l"));//muestra primera la posicion de la letra buscada
+console.log(s.indexOf("a",3)); // muestra la posicion de a apartie de la 3 posicion
+console.log(s.startsWith("salu"));
+console.log(s.endsWith("salu"));
+console.log(s.toLowerCase());
+console.log(s.toUpperCase());
+console.log(s.charAt(0));
+console.log(s.charAt(s.length-1));
+console.log(s.repeat(6));
+// se permite utilizar tambien [] de vez de usar charAt
+console.log(s[3]);
+console.log(s[s.length-1])
+
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
